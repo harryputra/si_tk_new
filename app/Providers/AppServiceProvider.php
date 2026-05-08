ae74506c197636d7f3ae2e980f97565c762f0b35
harryputra <108723310+harryputra@users.noreply.github.com>
2026-05-09 03:36:31 +0700

    fix: force https for production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         InboundPayment::observe(InboundPaymentObserver::class);
         OutboundRequest::observe(OutboundRequestObserver::class);
         Payroll::observe(PayrollObserver::class);
