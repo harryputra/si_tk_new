365c5c0da168c6dc0041f92a65e1e55ebfc45b44
harryputra <108723310+harryputra@users.noreply.github.com>
2026-05-09 03:19:22 +0700

    fix: use global fake() helper

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -12,13 +12,13 @@ class StudentFactory extends Factory
     public function definition(): array
     {
         return [
-            'nis' => fake()->unique()->numerify('#####'),
-            'nama_lengkap' => fake()->name(),
-            'nama_panggilan' => fake()->firstName(),
-            'tahun_angkatan' => fake()->randomElement([2022, 2023, 2024]),
-            'jenis_siswa' => fake()->randomElement(['reguler', 'reguler_opsi2', 'fullday']),
-            'nama_wali' => fake()->name(),
-            'no_hp_wali' => fake()->phoneNumber(),
+            'nis' => \fake()->unique()->numerify('#####'),
+            'nama_lengkap' => \fake()->name(),
+            'nama_panggilan' => \fake()->firstName(),
+            'tahun_angkatan' => \fake()->randomElement([2022, 2023, 2024]),
+            'jenis_siswa' => \fake()->randomElement(['reguler', 'reguler_opsi2', 'fullday']),
+            'nama_wali' => \fake()->name(),
+            'no_hp_wali' => \fake()->phoneNumber(),
             'status' => 'aktif',
         ];
     }
